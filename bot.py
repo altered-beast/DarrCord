b91546f442229c91711cbd6d90358b136763ad8e
altered-beast <axvantia@protonmail.com>
2022-02-09 22:27:22 +0000

    added permissions checking

--- bot.py
+++ bot.py
@@ -23,6 +23,7 @@ download_path = config["sonarr"]["download_path"]
 # discord configs
 discord_token = config["discord"]["token"]
 admin_user_id = config["discord"]["admin_user_id"]
+prefix = config["discord"]["prefix"]
 
 # logging
 logger = logging.getLogger("nextcord")
@@ -40,7 +41,6 @@ bot = commands.Bot(command_prefix="-", intents=intents)
 
 @bot.event
 async def on_ready():
-    on_ready.admin_user = await bot.fetch_user(admin_user_id)
 
     await PendingRequest.create_table(if_not_exists=True)
 
@@ -49,12 +49,16 @@ async def on_ready():
 @bot.command()
 async def request(
     ctx,
-    *series_name: str,
+    *raw_series_name: str,
 ):
+
+    series_name = ""
+    for item in raw_series_name:
+        series_name = series_name + "%20" + item
+
     if not series_name:
         await ctx.send("please specify a series name")
         return
-    series_name.replace(" ", "%20")
 
     results = False
     async with aiohttp.ClientSession() as session:
@@ -68,7 +72,7 @@ async def request(
         results = True
 
     if results:
-        view = views.SeriesSelectView(res, ctx, on_ready.admin_user)
+        view = views.SeriesSelectView(res, ctx, admin_user)
         await ctx.send("Choose a series from the dropdown:", view=view)
     else:
         await ctx.send("No results found for your request")
@@ -76,87 +80,104 @@ async def request(
 
 @bot.command()
 async def approve(ctx, *approve_select):
-    Pending_Requests = await PendingRequest.select()
+    admin_user = await bot.fetch_user(admin_user_id)
 
-    if approve_select and len(Pending_Requests) > 0:
-        series_raw = json.loads(
-            Pending_Requests[int(approve_select[0]) - 1]["series_data"]
-        )
+    if ctx.author.id == admin_user.id:
 
-        required_fields = [
-            "tvdbId",
-            "title",
-            "titleSlug",
-            "images",
-            "seasons",
-        ]
-        series = {
-            key: value for key, value in series_raw.items() if key in required_fields
-        }
-        # for quality profile may need to be configureable by admin
-        series["profileId"] = 1
-        series["rootFolderPath"] = download_path
-        series["addOptions"] = {
-            "ignoreEpisodesWithFiles": True,
-            "ignoreEpisodesWithoutFiles": False,
-            "searchForMissingEpisodes": True,
-        }
+        Pending_Requests = await PendingRequest.select()
 
-        async with aiohttp.ClientSession() as session:
-            async with session.post(
-                f"{sonarr_url}/api/series/?apikey={api_key}",
-                json=series,
-            ) as raw_response:
-                json_res = await raw_response.json()
-                print(json_res)
-
-                await ctx.send(
-                    f"adding :{Pending_Requests[int(approve_select[0]) - 1]['Title']}"
-                )
-                await PendingRequest.delete().where(
-                    PendingRequest.id
-                    == Pending_Requests[int(approve_select[0]) - 1]["id"]
-                )
-    elif len(Pending_Requests) > 0:
-        Pending_embed = nextcord.Embed()
-        for i in range(len(Pending_Requests)):
-            Pending_embed.add_field(
-                name=(f"{i+1}.{Pending_Requests[i]['Title']}"),
-                inline=False,
-                value=f"requested by: {Pending_Requests[i]['requester_name']}",
+        if approve_select and len(Pending_Requests) > 0:
+            series_raw = json.loads(
+                Pending_Requests[int(approve_select[0]) - 1]["series_data"]
             )
-        await ctx.send(embed=Pending_embed)
+
+            required_fields = [
+                "tvdbId",
+                "title",
+                "titleSlug",
+                "images",
+                "seasons",
+            ]
+            series = {
+                key: value
+                for key, value in series_raw.items()
+                if key in required_fields
+            }
+            # for quality profile may need to be configureable by admin
+            series["profileId"] = 1
+            series["rootFolderPath"] = download_path
+            series["addOptions"] = {
+                "ignoreEpisodesWithFiles": True,
+                "ignoreEpisodesWithoutFiles": False,
+                "searchForMissingEpisodes": True,
+            }
+
+            async with aiohttp.ClientSession() as session:
+                async with session.post(
+                    f"{sonarr_url}/api/series/?apikey={api_key}",
+                    json=series,
+                ) as raw_response:
+                    json_res = await raw_response.json()
+                    print(json_res)
+
+                    await ctx.send(
+                        f"adding :{Pending_Requests[int(approve_select[0]) - 1]['Title']}"
+                    )
+                    await PendingRequest.delete().where(
+                        PendingRequest.id
+                        == Pending_Requests[int(approve_select[0]) - 1]["id"]
+                    )
+        elif len(Pending_Requests) > 0:
+            Pending_embed = nextcord.Embed()
+            for i in range(len(Pending_Requests)):
+                Pending_embed.add_field(
+                    name=(f"{i+1}.{Pending_Requests[i]['Title']}"),
+                    inline=False,
+                    value=f"requested by: {Pending_Requests[i]['requester_name']}",
+                )
+            await ctx.send(
+                "please select an option to approve from below:", embed=Pending_embed
+            )
+        else:
+            await ctx.send("no pending requests")
     else:
-        await ctx.send("no pending requests")
+        await ctx.send("you are not authorised to run this command")
 
 
 @bot.command()
 async def deny(ctx, *deny_select):
-    Pending_Requests = await PendingRequest.select()
+    admin_user = await bot.fetch_user(admin_user_id)
+    if ctx.author.id == admin_user.id:
 
-    if deny_select and len(Pending_Requests) > 0:
+        Pending_Requests = await PendingRequest.select()
 
-        print(deny_select)
+        if deny_select and len(Pending_Requests) > 0:
 
-        await PendingRequest.delete().where(
-            PendingRequest.id == Pending_Requests[int(deny_select[0]) - 1]["id"]
-        )
+            print(deny_select)
 
-        await ctx.send(
-            f"Denied request for :{Pending_Requests[int(deny_select[0]) - 1]['Title']} "
-        )
-    elif len(Pending_Requests) > 0:
-        Pending_embed = nextcord.Embed()
-
-        for i in range(len(Pending_Requests)):
-            Pending_embed.add_field(
-                name=(f"{i+1}.{Pending_Requests[i]['Title']}"),
-                inline=False,
-                value=f"requested by: {Pending_Requests[i]['requester_name']}",
+            await PendingRequest.delete().where(
+                PendingRequest.id == Pending_Requests[int(deny_select[0]) - 1]["id"]
             )
-        await ctx.send(embed=Pending_embed)
+
+            await ctx.send(
+                f"Denied request for :{Pending_Requests[int(deny_select[0]) - 1]['Title']} "
+            )
+        elif len(Pending_Requests) > 0:
+            Pending_embed = nextcord.Embed()
+
+            for i in range(len(Pending_Requests)):
+                Pending_embed.add_field(
+                    name=(f"{i+1}.{Pending_Requests[i]['Title']}"),
+                    inline=False,
+                    value=f"requested by: {Pending_Requests[i]['requester_name']}",
+                )
+            await ctx.send(
+                "please select an option to deny from below:", embed=Pending_embed
+            )
+        else:
+            await ctx.send("no pending requests")
     else:
-        await ctx.send("no pending requests")
+        await ctx.send("you are not authorised to run this command")
 
 
 bot.run(discord_token)
